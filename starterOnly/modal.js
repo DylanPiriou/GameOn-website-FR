9f240f7259ae9a2f86cbfc0df5bdf606b9f6b73c
DylanPiriou <dylan.piriou@hotmail.com>
2023-09-21 16:16:59 +0200

    close modal feature & confirm message

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,44 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close")
+const btnSubmit = document.querySelector(".btn-submit")
+const confirmMessage = document.querySelector(".confirm-message");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// close modal event
+closeBtn.addEventListener("click", closeModal);
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+// launch form event
+btnSubmit.addEventListener("click", (e) => {
+  e.preventDefault();
+  closeModal();
+  setTimeout(() => {
+    handleDisplayMessage();
+  }, 300);
+});
+
+// display confirm message when form is submitted
+function handleDisplayMessage() {
+  confirmMessage.style.display = "block";
+  setTimeout(() => {
+    confirmMessage.style.display = "none";
+  }, 3000);
+}
+
+
+
+
+
 
 
